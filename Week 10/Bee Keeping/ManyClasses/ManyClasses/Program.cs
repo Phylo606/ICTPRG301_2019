95cd34967433a9db49c8d7cd3d46407badcd215f
MATTHEW PHYLAND <102119144@student.swin.edu.au>
2019-10-16 18:27:50 +1100

    week 10 complete

---
+++ Week 10/Bee Keeping/ManyClasses/ManyClasses/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ManyClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var studentClass = new StudentClass();
+            studentClass.Name = "Intro Programming";
+            studentClass.StartTime = "8:30";
+            studentClass.EndTime = "13:30";
+
+            var student1 = new Student();
+            student1.Age = 31;
+            student1.FirstName = "Bob";
+            student1.LastName = "Joe";
+            student1.HappyBirthday();
+
+            var student2 = new Student();
+            student2.Age = 564;
+            student2.FirstName = "Frank";
+            student2.LastName = "Pooped";
+
+        }
+    }
+
+    public class StudentClass
+    {
+        public string Name;
+        public string StartTime;
+        public string EndTime;
+
+        public List<Student> Students = new List<Student>();
+    }
+
+    public class Student
+    {
+        public string Id;
+        public string FirstName;
+        public string LastName;
+
+        public int Age;
+
+        public string PhoneNumber;
+
+        public void HappyBirthday()
+        {
+            Age = Age + 1;
+        }
+    }
+}
---
+++ Week 10/Todo/TodoFunctional/Program.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TodoFunctional
+{
+    class Program
+    {
+
+        //    static List<string> TodoTitles = new List<string>();
+        //    static List<bool> TodoCompleted = new List<bool>();
+        static List<Todo> TodoList = new List<Todo>();
+
+
+        static int AddTodo(string title, bool completed)
+        {
+            var index = TodoList.Count;
+            Todo t = new Todo(title, completed);
+            TodoList.Add(t);
+            //TodoTitles.Add(title);
+            //TodoCompleted.Add(completed);
+
+            return index;
+        }
+        static void RemoveTodo(int index)
+        {
+            TodoList.RemoveAt(index);
+        }
+
+        static void ToggleTodo(int index)
+        {
+            TodoList[index].Completed = !TodoList[index].Completed;
+        }
+
+        static void DisplayTodo(int index)
+        {
+            var title = TodoList[index].Title;
+            var completed = TodoList[index].Completed;
+
+            if (completed)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+
+            Console.WriteLine(" - " + title);
+            Console.ForegroundColor = ConsoleColor.White;
+
+        }
+        static void DrawSepearator()
+        {
+            Console.WriteLine(string.Concat(Enumerable.Repeat("-", Console.BufferWidth)));
+        }
+        static void DisplayAllTodos()
+        {
+            DrawSepearator();
+            for (int i = 0; i < TodoList.Count; i++)
+            {
+                DisplayTodo(i);
+            }
+
+            DrawSepearator();
+        }
+
+        static void ReadNewTodo()
+        {
+            Console.WriteLine("Add Todo: ");
+            //(!COMPLETE!) Do stuff in here to create a new Todo.
+            Console.WriteLine("Enter title of Todo: ");
+            string T = Console.ReadLine();
+            Console.WriteLine("Is " + "'" + T + "'" + " complete? (y or n)");
+            var K = Console.ReadKey();
+            if(K.Key == ConsoleKey.Y)
+            {
+                bool C = true;
+                AddTodo(T, C);
+            }
+            else if(K.Key == ConsoleKey.N)
+            {
+                bool C = false;
+                AddTodo(T, C);
+            }
+            
+        }
+
+        static void ReadToggleExistingTodo()
+        {
+            Console.WriteLine("Toggle Todo: ");
+            // Do something to toggle the todo item thingy-ma-bob thing
+            Console.WriteLine("Which Todo's status do you wish to toggle?");
+            var T = Console.ReadLine();
+            var Toggle = Convert.ToInt32(T);
+            ToggleTodo(Toggle);
+        }
+
+        static void ReadRemoveTodo()
+        {
+            Console.WriteLine("Remove Todo: ");
+            //(!COMPLETE!) Remove craptasic todos here
+            Console.WriteLine("Which entry in the Todo list do you wish to remove? (first entry is 0, second is 1 etc etc)");
+            var R = Console.ReadLine();
+            var Remove = Convert.ToInt32(R);
+            RemoveTodo(Remove);
+        }
+
+        public class Todo
+        {
+            public string Title;
+            public bool Completed;
+
+            public Todo(string title, bool completed)
+            {
+                Title = title;
+                Completed = completed;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Todo Application");
+
+            // Add a few example Todos
+            AddTodo("Get the shopping from the store because you a lazy dude and had to pre order it", false);
+            AddTodo("Kiss a tree because climate", false);
+            AddTodo("Finish this task because you are a model student", false);
+            AddTodo("Go to Intro Class this arvo", true);
+            AddTodo("Attended Hormone treatment", false);
+
+
+            while (true)
+            {
+                Console.Clear();
+                DisplayAllTodos();
+                Console.WriteLine("Pick an option:");
+                Console.WriteLine(" (1) -> Create Todo");
+                Console.WriteLine(" (2) -> Toggle Completed");
+                Console.WriteLine(" (3) -> Remove Todo");
+                Console.WriteLine(" (4) -> Exit");
+
+
+                var key = Console.ReadKey(true);
+
+                if(key.Key == ConsoleKey.D1)
+                {
+                    ReadNewTodo();
+                }
+                else if(key.Key == ConsoleKey.D2)
+                {
+                    ReadToggleExistingTodo();
+                }
+                else if(key.Key == ConsoleKey.D3)
+                {
+                    ReadRemoveTodo();
+                }
+                else if(key.Key == ConsoleKey.D4)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Nah you dumb, give me a valid answer please. Press any key to stop me raging at you....");
+                    Console.ReadKey(true);
+                }                
+
+            }
+
+        }
+    }
+}
