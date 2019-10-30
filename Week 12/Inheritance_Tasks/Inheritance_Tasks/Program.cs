fa15a98fabe33bc40afe1f9a1eb35f4dc1bc6a60
MATTHEW PHYLAND <102119144@student.swin.edu.au>
2019-10-30 18:43:01 +1100

    week 12

---
+++ Week 12/Inheritance_Tasks/Inheritance_Tasks/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Inheritance_Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello there!");
+            Console.WriteLine("What type of person would you like to create?");
+        }
+    }
+
+    public class Person
+    {
+        public string Name;
+        public int YearOfBirth;
+        public float Height;
+        public string Gender;
+        public int GetAge()
+        {
+            int age = 2019 - YearOfBirth;
+            return age;
+        }
+        
+    }
+
+    public class Student : Person
+    {
+        public string Id;
+        public DateTime EnrolledDate;
+        public List<string> Units;
+        public DateTime CalculateExpiryDate()
+        {
+            var ed = EnrolledDate.AddYears(2);
+            return ed;
+        }
+    }
+
+    public class Staff : Person
+    {
+        public string Id;
+        public string Office;
+        public string Email;
+    }
+
+    public class Admin: Staff
+    {
+        public int ClearanceLevel;
+        public string InternalPhoneNumber;
+        public bool TryAccess(int LevelNeeded)
+        {
+            if(ClearanceLevel >= LevelNeeded)
+            {
+                Console.WriteLine("Access granted");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Access denied");
+                return false;
+            }
+        }
+    }
+
+    public class Teacher : Staff
+    {
+        public List<string> Units;
+        public DateTime ContractEndDate;
+        public bool HasContractExpired()
+        {
+            if (DateTime.Now > ContractEndDate)
+            {
+                Console.WriteLine("Contract has expired");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Contract is valid");
+                return false;
+            }
+        }
+    }
+}
---
+++ Week 12/animalexample/animalexample/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+
+namespace animalexample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Animal> myAnimals = new List<Animal>;
+            Console.WriteLine("Hello World!");
+            Animal myAnimal = new Animal(56, "Crane");
+
+            myAnimals.Add(myAnimal);
+            
+            Fish myFish = new Fish(1,"Fishy Fish", 56, true);
+
+            myAnimals.Add(myFish);
+
+            Tuna myTuna = new Tuna();
+            myTuna.Name = "bob";
+            myAnimals.Add(myTuna);
+
+            foreach(var animal in myAnimals)
+            {
+                if(animal is Fish) var MyFishy = animal as Fish)
+            }
+
+        }
+    }
+
+    public class Animal
+    {
+        public int Age;
+        public string Gender;
+
+        protected void HelloWorld()
+        {
+            Console.WriteLine("Hello World");
+        }
+
+        protected Animal(int age, string gender)
+        {
+            this.Age = age;
+            this.Gender = gender;
+        }
+
+    }
+
+    public class Fish : Animal
+    {
+        public float Size;
+        public bool CanEat;
+
+
+        public Fish(int age, string gender, float size, bool caneat) : base(age, gender)
+        {
+            this.Size = size;
+            this.CanEat = caneat;
+        }
+    }
+
+    public class Zebra: Animal
+    {
+        public bool IsWild;
+    }
+
+    public class Tuna : Fish
+    {
+        public string Name;
+    }
+}
