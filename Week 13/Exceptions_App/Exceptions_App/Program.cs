62f977904f0cf0a11779e4cb7a759c913d5276b9
MATTHEW PHYLAND <102119144@student.swin.edu.au>
2019-11-13 17:38:33 +1100

    Week 13 exceptions app complete

---
+++ Week 13/Exceptions_App/Exceptions_App/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Exceptions_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+
+                Console.WriteLine("Hello World!");
+                Console.WriteLine("Enter an integer!");
+                try
+                {
+                    int integer1 = int.Parse(Console.ReadLine());
+                    int sum = 0;
+                    for (int n = integer1; n > 0; sum += n % 10, n /= 10) ;
+
+                    if (integer1 <= 0)
+                    {
+                        throw new LessThanZeroException();
+                    }
+                    else if (integer1 > 15)
+                    {
+                        throw new GreaterThanFifteenException();
+                    }
+                    else
+                    {
+                        Console.WriteLine(sum);
+                        Console.WriteLine("Time: " + DateTime.Now);
+                    }
+
+
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Error: You didn't enter an integer.");
+                    Console.WriteLine("Time: " + DateTime.Now);
+                }
+                catch (LessThanZeroException)
+                {
+                    Console.WriteLine("Error: Your input was less than or equal to zero.");
+                    Console.WriteLine("Time: " + DateTime.Now);
+                }
+                catch (GreaterThanFifteenException)
+                {
+                    Console.WriteLine("Error: Your input was greater than fifteen."); ;
+                    Console.WriteLine("Time: " + DateTime.Now);
+                }
+            }
+        }
+    }
+
+    class LessThanZeroException : Exception
+    {
+        
+    }
+    class GreaterThanFifteenException : Exception
+    {
+
+    }
+}
